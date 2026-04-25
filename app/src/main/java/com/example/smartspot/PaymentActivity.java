388e7805fc3e9d554b413f92ac1ae3f1f96b463c
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-04-26 00:10:03 +0530

    Logic Added

--- app/src/main/java/com/example/smartspot/PaymentActivity.java
+++ app/src/main/java/com/example/smartspot/PaymentActivity.java
@@ -1,26 +1,37 @@
 package com.example.smartspot;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class PaymentActivity extends AppCompatActivity {
 
+    RadioGroup paymentGroup;
+    Button continueBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_payment);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        paymentGroup = findViewById(R.id.paymentGroup);
+        continueBtn = findViewById(R.id.continueBtn);
+
+        continueBtn.setOnClickListener(v -> {
+
+            int selectedId = paymentGroup.getCheckedRadioButtonId();
+
+            if (selectedId == -1) {
+                Toast.makeText(this, "Select a payment method", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Intent intent = new Intent(PaymentActivity.this, ConfirmationActivity.class);
+            startActivity(intent);
         });
     }
 }
