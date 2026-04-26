d8fce9a6e81b737401e045a29e7b7fad02e52408
Nischay Mehta <mehtanischay2006@gmail.com>
2026-04-26 13:57:01 +0530

    Active booking

--- app/src/main/java/com/example/smartspot/api/ApiClient.java
+++ app/src/main/java/com/example/smartspot/api/ApiClient.java
@@ -1,18 +1,18 @@
 package com.example.smartspot.api;
 
-  
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
-  
-    private static final String BASE_URL = "http://10.0.2.2:3000/";
-  
-    // Real phone URL (PUT YOUR PC IP HERE)
+
+    // 1. Emulator URL
+    private static final String EMULATOR_URL = "http://10.0.2.2:3000/";
+
+    // 2. Real phone URL (Update this with your PC's IP)
     private static final String REAL_DEVICE_URL = "http://10.98.104.72:3000/";
 
-    // 👉 CHANGE THIS WHEN NEEDED
+    // 👉 CHANGE THIS TO EITHER EMULATOR_URL OR REAL_DEVICE_URL
     private static final String BASE_URL = REAL_DEVICE_URL;
-
 
     private static Retrofit retrofit;
 
