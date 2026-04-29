e74438a8e6f05c4ab92129350f1ce806edbd2f77
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-04-29 23:53:56 +0530

    Made ip address global

--- app/src/main/java/com/example/smartspot/api/ApiClient.java
+++ app/src/main/java/com/example/smartspot/api/ApiClient.java
@@ -5,10 +5,10 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
   
-    private static final String BASE_URL = "http://10.0.2.2:3000/";
+//    public static final String BASE_URL = "http://10.0.2.2:3000/";
   
 
-//    public static final String BASE_URL = "http://10.40.36.26:3000/";
+    public static final String BASE_URL = "http://10.40.36.26:3000/";
 
 
 
